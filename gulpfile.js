17ed688159887ac04b0487a223825b42277bf173
wangsongiam <wangsongiam@gmail.com>
2016-09-30 15:05:58 -0400

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: './app'
+  });
+});
+
+gulp.task('default', ['server'], function () {
+  gulp.watch('./app/**.html').on('change', reload)
+});
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+// });
