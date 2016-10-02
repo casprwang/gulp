af4053e38e061558c460f6e26466134e17486133
wangsongiam <wangsongiam@gmail.com>
2016-10-02 00:11:19 -0400

    gulp reload html and css

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ var reload      = browserSync.reload;
 
 gulp.task('server', function() {
   browserSync.init({
-    server: './app/html2'
+    server: './app'
   });
 });
 
 gulp.task('default', ['server'], function () {
-  gulp.watch('./app/html2/index2.html').on('change', reload)
+  gulp.watch('./app/index2.html').on('change', reload)
+  gulp.watch('./app/css/*.css').on('change', reload)
 });
 
 // gulp.task('browser-sync', function() {
