91a3de08efb3a06008c9a5b8d27eecf52f4342d3
wangsongiam <wangsongiam@gmail.com>
2016-11-01 04:10:32 -0400

    rebase

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
-
-
-gulp.task('server', function() {
-  browserSync.init({
-    server: './app'
-  });
-});
-
-gulp.task('default', ['server'], function () {
-  gulp.watch('./app/index.html').on('change', reload)
-  gulp.watch('./app/css/*.css').on('change', reload)
-});
-
-// sakljfjdska
-//test
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var reload      = browserSync.reload;
+//
+//
+// gulp.task('server', function() {
+//   browserSync.init({
+//     server: './app'
+//   });
+// });
+//
+// gulp.task('default', ['server'], function () {
+//   gulp.watch('./app/index.html').on('change', reload)
+//   gulp.watch('./app/css/*.css').on('change', reload)
+// });
+//
+// // sakljfjdska
+// //test
